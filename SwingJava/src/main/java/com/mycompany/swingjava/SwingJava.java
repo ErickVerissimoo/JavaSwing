2872cbf0b9e73d8f37b31c792195f292e925ee09
ErickVerissimoo <erickverissimodasilva144@gmail.com>
2024-08-04 21:45:24 -0300

    commit inicial

---
+++ SwingJava/src/main/java/com/mycompany/swingjava/SwingJava.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.swingjava;
+
+/**
+ *
+ * @author Erick 
+ */
+public class SwingJava {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
